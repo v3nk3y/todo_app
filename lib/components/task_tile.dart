d45dbd0224461a8ae4094bcee89188d7fdf9135a
venkatesh <nagireddyvenkatesh@gmail.com>
2020-08-11 15:43:17 -0400

    Checkbox update using local state management

--- lib/components/task_tile.dart
+++ lib/components/task_tile.dart
@@ -5,10 +5,27 @@ class TaskTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListTile(
       title: Text('List item goes here.'),
-      trailing: Checkbox(
-        value: false,
-        onChanged: null,
-      ),
+      trailing: TaskCheckBox(),
+    );
+  }
+}
+
+class TaskCheckBox extends StatefulWidget {
+  @override
+  _TaskCheckBoxState createState() => _TaskCheckBoxState();
+}
+
+class _TaskCheckBoxState extends State<TaskCheckBox> {
+  bool isChecked = false;
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: isChecked,
+      onChanged: (newVal) {
+        setState(() {
+          isChecked = newVal;
+        });
+      },
     );
   }
 }

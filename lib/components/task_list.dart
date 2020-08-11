69db9f828967b61074236aa3769bc20d1c6e4a76
venkatesh <nagireddyvenkatesh@gmail.com>
2020-08-11 19:09:25 -0400

    Make use Consumer widget to listen for task data changes

--- lib/components/task_list.dart
+++ lib/components/task_list.dart
@@ -6,18 +6,23 @@ import 'package:provider/provider.dart';
 class TaskList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        return TaskTile(
-            isChecked: Provider.of<TaskData>(context).tasks[index].isDone,
-            taskTitle: Provider.of<TaskData>(context).tasks[index].name,
-            checkBoxCallBack: (checkBoxCurrentState) {
+    return Consumer<TaskData>(
+      builder: (context, taskData, child) {
+        return ListView.builder(
+          itemBuilder: (context, index) {
+            return TaskTile(
+              isChecked: taskData.tasks[index].isDone,
+              taskTitle: taskData.tasks[index].name,
+              checkBoxCallBack: (checkBoxCurrentState) {
 //              setState(() {
 //                Provider.of<TaskData>(context).tasks[index].toggleDone();
 //              });
-            });
+              },
+            );
+          },
+          itemCount: taskData.tasks.length,
+        );
       },
-      itemCount: Provider.of<TaskData>(context).tasks.length,
     );
   }
 }

c1a4caf5cd0e0c24af759df1d0d8c352a43cc713
venkatesh <nagireddyvenkatesh@gmail.com>
2020-08-11 19:48:45 -0400

    Update task status added

--- lib/components/task_list.dart
+++ lib/components/task_list.dart
@@ -10,13 +10,12 @@ class TaskList extends StatelessWidget {
       builder: (context, taskData, child) {
         return ListView.builder(
           itemBuilder: (context, index) {
+            final task = taskData.tasks[index];
             return TaskTile(
-              isChecked: taskData.tasks[index].isDone,
-              taskTitle: taskData.tasks[index].name,
+              isChecked: task.isDone,
+              taskTitle: task.name,
               checkBoxCallBack: (checkBoxCurrentState) {
-//              setState(() {
-//                Provider.of<TaskData>(context).tasks[index].toggleDone();
-//              });
+                taskData.updateTask(task);
               },
             );
           },
